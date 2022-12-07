0bc5bc694f2f3b9138bace7b959dc7022716492b
Jev Forsberg <jevf@turbinelabs.com>
2022-12-07 10:09:01 -0700

    master/ update url on load

--- react_client/src/App.js
+++ react_client/src/App.js
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react";
 import { useGetData } from "./apiCalls";
 import "./App.css";
 
 const App = () => {
+  const [route, setRoute] = useState("/");
+
+  useEffect(() => {
+    const { data, isLoading, error } = useGetData({
+      url: `http://localhost:4000${route}`,
+    });
+  }, []);
+
   return <div className="App">Put ur React here, dingus...</div>;
 };
 
