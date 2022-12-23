5fcf1d717f14cc23c20a42090235dff5e97c4b80
Jev Forsberg <jevf@turbinelabs.com>
2022-12-22 18:46:14 -0700

    master/ remove console.logs from crud itils

--- node_postgres_api/db.CRUD.utilities.js
+++ node_postgres_api/db.CRUD.utilities.js
@@ -17,22 +17,16 @@ export const buildUpdateData = (body) => {
 // JEV: annotate return type here
 export const buildCreateTableData = (attrs) => {
   const parsedAttrs = JSON.parse(attrs);
-  console.log(attrs, "attrs");
   let finalStringifiedAttrs = "";
   const columnNames = Object.keys(parsedAttrs);
-  console.log(columnNames, "columnNames");
   for (const colName of columnNames) {
-    console.log(colName, "colName");
-    console.log(parsedAttrs[colName], "parsedAttrs[colName]");
     finalStringifiedAttrs =
       finalStringifiedAttrs +
       `${colName} ${parsedAttrs[colName]["type"]} ${
         parsedAttrs[colName]["nullStatus"] ?? ""
       }, `;
   }
-  console.log(finalStringifiedAttrs, "finalStringifiedAttrs");
 
   // Trim off dangling comma and whitespace
   return finalStringifiedAttrs.slice(0, -2);
-  // npx run-func db.CRUD.js createTable 'friends' <shell_variable>
 };
