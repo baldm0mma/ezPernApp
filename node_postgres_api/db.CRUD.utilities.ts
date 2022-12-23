399c2e6a085abd83d587076163edf421ec1d3611
Jev Forsberg <jevf@turbinelabs.com>
2022-12-22 18:59:18 -0700

    master/ add typescript

---
+++ node_postgres_api/db.CRUD.utilities.ts
@@ -0,0 +1,44 @@
+type BuildInsertDataArgsType = { [key: string]: any };
+
+type BuildInsertDataReturnType = {
+  stringifiedKeys: string;
+  stringifiedValues: string;
+};
+
+export const buildInsertData = (
+  attrs: BuildInsertDataArgsType
+): BuildInsertDataReturnType => {
+  const keys = Object.keys(attrs);
+  const stringifiedKeys = keys.join(", ");
+  const stringifiedValues = keys.map((key) => attrs[key]).join("', '");
+  return { stringifiedKeys, stringifiedValues };
+};
+
+type BuildUpdateDataArgsType = { [key: string]: any };
+
+export const buildUpdateData = (attrs: BuildUpdateDataArgsType): string => {
+  const keys = Object.keys(attrs);
+  const data = keys.map((key) => `${key} = '${attrs[key]}'`);
+  const stringifiedData = data.join(", ");
+  return stringifiedData;
+};
+
+type BuildCreateTableDataArgsType = string;
+
+export const buildCreateTableData = (
+  attrs: BuildCreateTableDataArgsType
+): string => {
+  const parsedAttrs = JSON.parse(attrs);
+  let finalStringifiedAttrs = "";
+  const columnNames = Object.keys(parsedAttrs);
+  for (const colName of columnNames) {
+    finalStringifiedAttrs =
+      finalStringifiedAttrs +
+      `${colName} ${parsedAttrs[colName]["type"]} ${
+        parsedAttrs[colName]["nullStatus"] ?? ""
+      }, `;
+  }
+
+  // Trim off dangling comma and whitespace
+  return finalStringifiedAttrs.slice(0, -2);
+};
